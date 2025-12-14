349c4f7edf0341e619ead5d2ad09cebdecf6b97f
dineshkhade123 <khadedinesh722@gmail.com>
2025-12-14 15:52:08 +0530

    add task 4

---
+++ Day4/task4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+<< about
+
+This script takes user input, input from arguments, and prints the variables.
+about
+
+
+
+# Argument 
+
+name=$1
+
+read -p "enter your fav game :" game 
+
+# Printing variables 
+
+echo "Your name : $name"
+echo "Fav Game : $game"
+
+
+
