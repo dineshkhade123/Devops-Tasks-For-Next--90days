df6bb58ab3885c095cadf491323b9436fca94524
Dinesh <khadedinesh722@gmail.com>
2025-12-15 16:32:29 +0530

    Create task1.sh

---
+++ Day5/task1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+<< Task
+This script creates multiple directories using arguments.
+Usage:
+./createDirectories.sh <dir_name> <start_number> <end_number>
+Task
+
+# Check if exactly 3 arguments are provided
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <dir_name> <start_number> <end_number>"
+    exit 1
+fi
+
+# Assign arguments
+dir_name=$1
+start_no=$2
+end_no=$3
+
+# Create directories using loop
+for ((i=start_no; i<=end_no; i++))
+do
+    mkdir "${dir_name}${i}"
+    echo "Created directory: ${dir_name}${i}"
+done
